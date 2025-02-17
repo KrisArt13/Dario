47c5f9861fe367751b52080ae1df75b423f5c516
KrisArt13 <113379636+KrisArt13@users.noreply.github.com>
2025-02-17 17:07:33 -0500

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    base: '/eldariportaf',
+    outDir: './dist' // Asegurar que la salida esté en "dist"
+  });
